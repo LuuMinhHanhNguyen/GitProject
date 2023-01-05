df5a748b2d14ca67669c1b8a2160ffb5e9518285
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2023-01-05 21:48:39 +0700

    n

--- variable_type_operator/currency_exchange.js
+++ variable_type_operator/currency_exchange.js
@@ -1,26 +1,30 @@
 let inputNumber = document.getElementById('inputNumber');
-
-let result1 = document.getElementById("result")
 let selectFrom = document.getElementById("selectFrom")
 let selectTo = document.getElementById("selectTo")
+let outputNumber = document.getElementById("result")
+function result1() {
 
-// step 1
-inputNumber.addEventListener('input',handleData)
+    let cal;
+    if(selectFrom.value.length === ""){
+        console.log(selectFrom.value)
+        outputNumber.innerText = "Please input your currency amount!"
+    }
+    if (selectFrom.value === "USD" && selectTo.value === "VND") {
+        cal = parseInt(inputNumber.value) * 23000
+        outputNumber.innerText = cal + " " + "VND"
+    }
+    if (selectFrom.value === "VND" && selectTo.value === "USD") {
+        cal = parseInt(inputNumber.value) / 23000
+        outputNumber.innerText = cal + " " + "USD"
+    }
+    if (selectFrom.value === "VND" && selectTo.value === "VND") {
+        cal = parseInt(inputNumber.value)
+        outputNumber.innerText = cal + " " + "VND"
+    }
+    if (selectFrom.value === "USD" && selectTo.value === "USD") {
+        cal = parseInt(inputNumber.value)
+        outputNumber.innerText = `${cal} USD`
+    }
 
-// step 2
-function handleData(ev){
-    let doMath;
-    let theInput = ev.target
-    console.log(typeof ev)
-    console.log(typeof ev.target)
-    console.log(theInput.value, typeof theInput.value)
-    doMath = parseInt(theInput.value) * 10
-    result1.textContent = doMath
 }
 
-
-function result(){
-    let input = inputNumber.value;
-
-    result1.innerHTML = parseInt(input * 23000)
-}
