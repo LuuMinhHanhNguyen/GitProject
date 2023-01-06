b2c61005a512e9ea476808ebfe4bb80eac25dfd0
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2023-01-06 19:50:07 +0700

    variable-dataTypes-operator

---
+++ variable_type_operator/calculator.js
@@ -0,0 +1,25 @@
+let display = document.querySelector("#display");
+let buttons = document.querySelectorAll("button")
+let lightGreen = "#90EE90";
+let white = "#FFFFFF";
+buttons.forEach((item) =>{
+    item.onclick = () => {
+        item.style.background = lightGreen;
+        setTimeout(() =>{ item.style.background = white }, 100)
+        if(item.id === "clear"){
+            display.value = ""
+        }
+        else if(display.value !== "" && item.id === "equal"){
+            display.value = eval(display.value)
+        }
+        else if(display.value === "" && item.id === "equal"){
+            display.value = "Empty!"
+            setTimeout(()=>{display.value = ""}, 200)
+        }
+        else{
+            display.value += item.id
+        }
+    }
+})
+
+
--- variable_type_operator/currency_exchange.js
+++ variable_type_operator/currency_exchange.js
@@ -2,29 +2,31 @@ let inputNumber = document.getElementById('inputNumber');
 let selectFrom = document.getElementById("selectFrom")
 let selectTo = document.getElementById("selectTo")
 let outputNumber = document.getElementById("result")
-function result1() {
+let button = document.getElementById("button");
+function displayResult() {
 
     let cal;
-    if(selectFrom.value.length === ""){
-        console.log(selectFrom.value)
-        outputNumber.innerText = "Please input your currency amount!"
-    }
-    if (selectFrom.value === "USD" && selectTo.value === "VND") {
-        cal = parseInt(inputNumber.value) * 23000
-        outputNumber.innerText = cal + " " + "VND"
-    }
-    if (selectFrom.value === "VND" && selectTo.value === "USD") {
-        cal = parseInt(inputNumber.value) / 23000
-        outputNumber.innerText = cal + " " + "USD"
-    }
-    if (selectFrom.value === "VND" && selectTo.value === "VND") {
-        cal = parseInt(inputNumber.value)
-        outputNumber.innerText = cal + " " + "VND"
-    }
-    if (selectFrom.value === "USD" && selectTo.value === "USD") {
-        cal = parseInt(inputNumber.value)
-        outputNumber.innerText = `${cal} USD`
-    }
+   if(inputNumber.value !== ""){
+       if (selectFrom.value === "USD" && selectTo.value === "VND") {
+           cal = parseInt(inputNumber.value) * 23000
+           outputNumber.innerText = cal + " " + "VND"
+       }
+       if (selectFrom.value === "VND" && selectTo.value === "USD") {
+           cal = parseInt(inputNumber.value) / 23000
+           outputNumber.innerText = cal + " " + "USD"
+       }
+       if (selectFrom.value === "VND" && selectTo.value === "VND") {
+           cal = parseInt(inputNumber.value)
+           outputNumber.innerText = cal + " " + "VND"
+       }
+       if (selectFrom.value === "USD" && selectTo.value === "USD") {
+           cal = parseInt(inputNumber.value)
+           outputNumber.innerText = `${cal} USD`
+       }
+   } else {
+       outputNumber.innerHTML = "Please enter your currency amount!"
+   }
+
 
 }
 
